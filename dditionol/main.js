893b8d1f43e2863e1917d6b6c440ba225eb6e217
bodya1710 <bodyabordiyk@gmail.com>
2021-11-24 12:01:44 +0200

    p

---
+++ dditionol/main.js
@@ -0,0 +1,29 @@
+// //- Дано натуральное число n. Выведите все числа от 1 до n.
+// let cautn = (n) =>{
+//     for (let i = 1; i <= n; i++) {
+//         console.log(i)
+//     }
+// }
+// cautn(6);
+//- Даны два целых числа A и В . Выведите все числа от A до B включительно, в порядке возрастания,
+// если A < B, или в порядке убывания в противном случае.
+// function cauntN(a, b, callback) {
+//     callback(a, b);
+// }
+// function f1(a, b) {
+//     if (a < b){
+//         for (let i = a; i <= b; i++) {
+//             console.log(i)
+//         }
+//     }
+// }
+// function f2(a, b) {
+//     if (!(a > b)){
+//         for (let i = b; i >= a; i--) {
+//             console.log(i);
+//         }
+//     }
+// }
+// cauntN( 8, 18, f1);
+// cauntN( 8, 18, f2);
+
